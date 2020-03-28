8a774339f7ed5371fb956059226cb72a6eca91dd
Thomas Haller <Thomas.Haller@lab10.coop>
2020-03-28 19:56:32 +0100

    deployment for sigma1 and tau1

--- truffle-config.js
+++ truffle-config.js
@@ -1,4 +1,37 @@
+const TestRPC = require("ganache-cli");
+
+const fs = require('fs');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const testmnemonic = fs.readFileSync(".mnemonic").toString().trim();
+
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
+  networks: {
+    development: {
+      provider: TestRPC.provider({port: 7545}),
+      network_id: "*" // Match any network id
+    },
+
+    tau1: {
+      provider: () => new HDWalletProvider(testmnemonic, `https://rpc.tau1.artis.network`),
+      //host: "116.203.118.82",     // Localhost (default: none)
+      //host: "127.0.0.1",
+      //port: 8545,            
+      network_id: "*",       // Any network (default: none)
+      gas: 4000000,
+      gasPrice: 100000000000
+     },
+
+     sigma1: {
+      provider: () => new HDWalletProvider(testmnemonic, `https://rpc.sigma1.artis.network`),
+      //host: "116.203.118.82",     // Localhost (default: none)
+      //host: "127.0.0.1",
+      //port: 8545,            
+      network_id: "*",       // Any network (default: none)
+      gasPrice: 1000000000
+     },
+
+  }
 };
--- truffle.js
+++ truffle.js
@@ -1,10 +1,35 @@
 const TestRPC = require("ganache-cli");
 
+const fs = require('fs');
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const testmnemonic = fs.readFileSync(".mnemonic").toString().trim();
+
 module.exports = {
   networks: {
     development: {
       provider: TestRPC.provider({port: 7545}),
       network_id: "*" // Match any network id
-    }
+    },
+
+    tau1: {
+      provider: () => new HDWalletProvider(testmnemonic, `wss://ws.tau1.artis.network`),
+      //host: "116.203.118.82",     // Localhost (default: none)
+      //host: "127.0.0.1",
+      //port: 8545,            
+      network_id: "*",       // Any network (default: none)
+      gas: 500000,
+      gasPrice: 100000000000
+     },
+
+     sigma1: {
+      provider: () => new HDWalletProvider(testmnemonic, `wss://ws.sigma1.artis.network`),
+      //host: "116.203.118.82",     // Localhost (default: none)
+      //host: "127.0.0.1",
+      //port: 8545,            
+      network_id: "*",       // Any network (default: none)
+      gas: 500000,
+      gasPrice: 100000000000
+     },
+
   }
 };
